c683fb14ff33aaf448b2f5043e352f39e6c9bff6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-11-29 14:49:10 +0200

    Rename variables and methods in TennisGame

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -13,57 +13,54 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.player1_points = 0
-        self.player2_points = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
-            self.player1_points = self.player1_points + 1
+            self.player1_score = self.player1_score + 1
         else:
-            self.player2_points = self.player2_points + 1
+            self.player2_score = self.player2_score + 1
 
-    def _get_even_points_score(self):
-        if self.player1_points == SCORE.LOVE.value:
+    def _get_tied_score(self):
+        if self.player1_score == SCORE.LOVE.value:
             return "Love-All"
-        if self.player1_points == SCORE.FIFTEEN.value:
+        if self.player1_score == SCORE.FIFTEEN.value:
             return "Fifteen-All"
-        if self.player1_points == SCORE.THIRTY.value:
+        if self.player1_score == SCORE.THIRTY.value:
             return "Thirty-All"
-
         return "Deuce"
 
-    def _get_beyond_deuce_score(self):
-        points_difference = self.player1_points - self.player2_points
+    def _get_advantage_or_win_score(self):
+        score_difference = self.player1_score - self.player2_score
 
-        if points_difference == 1:
+        if score_difference == 1:
             return f"Advantage {self.player1_name}"
-        if points_difference == -1:
+        if score_difference == -1:
             return f"Advantage {self.player2_name}"
-        if points_difference >= 2:
+        if score_difference >= 2:
             return f"Win for {self.player1_name}"
-
         return f"Win for {self.player2_name}"
 
-    def _get_score_name(self, points):
+    def _point_to_score_name(self, points):
         if points == SCORE.LOVE.value:
             return "Love"
         if points == SCORE.FIFTEEN.value:
             return "Fifteen"
         if points == SCORE.THIRTY.value:
             return "Thirty"
-
         return "Forty"
 
-    def _get_before_deuce_score(self):
-        player1_score = self._get_score_name(self.player1_points)
-        player2_score = self._get_score_name(self.player2_points)
-        return f"{player1_score}-{player2_score}"
+    def _get_uneven_score(self):
+        player1_score_name = self._point_to_score_name(self.player1_score)
+        player2_score_name = self._point_to_score_name(self.player2_score)
+        return f"{player1_score_name}-{player2_score_name}"
 
     def get_score(self):
-        if self.player1_points == self.player2_points:
-            return self._get_even_points_score()
+        if self.player1_score == self.player2_score:
+            return self._get_tied_score()
 
-        if self.player1_points >= SCORE.GAME.value or self.player2_points >= SCORE.GAME.value:
-            return self._get_beyond_deuce_score()
+        if self.player1_score >= SCORE.GAME.value or self.player2_score >= SCORE.GAME.value:
+            return self._get_advantage_or_win_score()
 
-        return self._get_before_deuce_score()
+        return self._get_uneven_score()
