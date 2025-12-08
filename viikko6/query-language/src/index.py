56acd2ed9de19edfe38aa713ae5e1fdae4e8a893
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-12-08 17:05:11 +0200

    Add QueryBuilder class that returns all players

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,48 +1,18 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, Not, All, Or, HasAtLeast, PlaysIn, HasFewerThan
+from query_builder import QueryBuilder
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
-
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
-
-    matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
-    )
-
-    matcher = Or(
-        HasAtLeast(45, "goals"),
-        HasAtLeast(70, "assists")
-    )
-
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("COL"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
-    )
+    query = QueryBuilder()
+    matcher = query.build()
 
     for player in stats.matches(matcher):
         print(player)
-
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))  # 899
 
 
 if __name__ == "__main__":
---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,9 @@
+from matchers import All
+
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def build(self):
+        return self._matcher
